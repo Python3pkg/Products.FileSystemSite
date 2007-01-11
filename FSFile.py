d8049ea478d95033f13b17927e0672302892cffc
Jasper Op de Coul <opdecoul@ubib.eur.nl>
2007-01-11 15:26:19 +0000

    Fixed deprecation warning, breaking compatibility with zope versions prior to 2.10

--- FSFile.py
+++ FSFile.py
@@ -21,10 +21,8 @@ from AccessControl import ClassSecurityInfo
 from DateTime import DateTime
 from OFS.Cache import Cacheable
 from OFS.Image import File
-try:
-    from zope.app.content_types import guess_content_type
-except ImportError: # BBB: for Zope < 2.9
-    from OFS.content_types import guess_content_type
+
+from zope.contenttype import guess_content_type
 
 from DirectoryView import registerFileExtension
 from DirectoryView import registerMetaType
